6c806741cffc4a09bacf712e395aaffe8dff985f
Arthur Moreira <arthurmcs96@gmail.com>
2022-04-14 14:43:48 -0300

    implements function fetch data from json file

---
+++ src/services/docApi.js
@@ -0,0 +1,9 @@
+
+const getDocs=()=> {
+    const response = fetch('documents.json')
+      .then((res) => res.json())
+      .then((json) => json.documents)
+    return response
+  };
+
+export default getDocs;
